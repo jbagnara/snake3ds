ebe33e378a610dcbde99ce2e2dad73b98b71cd7e
jbagnara <jbagnara@purdue.edu>
2019-08-02 15:35:48 -0500

    significantly improved readability and modularity

--- main.cpp
+++ main.cpp
@@ -6,37 +6,29 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+int blockSize = 11;	//size of blocks by pixel
+int xRes = 400;
+int yRes = 240;
 
-
-void block(int y, int x, int color, u8* fb, u8* fb1) //print snake
-{
+void drawPixel(int y, int x, int color, u8* fb, u8* fb1, u8 value) { //draw pixel of specified color
 	//0 is B, 1 is G, 2 is R
-        fb[3*(y+x*240)+color] = 0xFF;
-	fb1[3*(y+x*240)+color] = 0xFF;
+        fb[3*(y+x*240)+color] = value;
+	fb1[3*(y+x*240)+color] = value;
 }
 
-void remove(int y, int x, u8* fb, u8* fb1) //remove snake tail
-{
-	fb[3*(y+x*240)]=0x00;
-	fb[3*(y+x*240)+1]=0x00;
-	fb[3*(y+x*240)+2]=0x00;
-
-	fb1[3*(y+x*240)]=0x00;
-        fb1[3*(y+x*240)+1]=0x00;
-        fb1[3*(y+x*240)+2]=0x00;
-}
-
-void wall(int limitstarty, int limitstartx, int limitendy, int limitendx, u8* fb, u8* fb1) //wallblock
-{
-
-    	for(int c = 0; c<240; c++)
-        {
-                for(int b = 0; b<400; b++)
-                {
-                	if((c<limitstarty||c>=limitendy)||(b<limitstartx||b>=limitendx)){
-                		block(c, b, 0, fb, fb1);}
-	        }
+void drawBlock(int y, int x, int color, u8* fb, u8* fb1, u8 value) {	//fills block of pixels with color
+	for(int b=0; b<blockSize; b++){
+		for(int c=0; c<blockSize; c++){
+			drawPixel(y+b, x+c, color, fb, fb1, value);
+			
+		}
 	}
+}
+
+void eraseBlock(int y, int x, u8* fb, u8* fb1) {	//erases entire block
+	drawBlock(y, x, 0, fb, fb1, 0x00);
+	drawBlock(y, x, 1, fb, fb1, 0x00);
+	drawBlock(y, x, 2, fb, fb1, 0x00);
 }
 
 void save(int score)
@@ -57,39 +49,97 @@ void save(int score)
 		writefile.close();
 	}
 }
+
+class playField {
+	public:
+		playField(int, int);
+		void init(u8*, u8*);
+
+		int xLen;	//x resolution	(400 for 3ds)
+		int yLen;	//y resulution	(240 for 3ds)
+		int limitStartx, limitStarty;	//x, y coordinate where field starts
+		int limitendy, limitendx;	//x, y coordinate where field ends
+		int xArrLen, yArrLen;	//size of block array (field)
+
+};
+
+playField::playField(int xLen, int yLen){
+	this->xLen = xLen;
+	this->yLen = yLen;	
+	limitStarty=((yLen%blockSize)/2);	
+	limitStartx=((xLen%blockSize)/2);
+	limitendy=limitStarty+(blockSize*(yLen/blockSize));
+	limitendx=limitStartx+(blockSize*(xLen/blockSize));
+	xArrLen=(limitStartx-limitendx)/blockSize;
+	yArrLen=(limitStarty-limitendy)/blockSize;
+
 	
+
+}
+
+void playField::init(u8* fb, u8* fb1){	//initializes playfield and draws walls
+    	for(int y = 0; y<yLen; y++) {
+                for(int x = 0; x<xLen; x++) {
+                	if((y<limitStarty||y>=limitendy)||(x<limitStartx||x>=limitendx)){
+                		drawPixel(y, x, 0, fb, fb1, 0xFF);
+			}
+	        }
+	}
+}
+
+class gameState {
+	public:
+		int headx, heady;	//snake head coordinates
+		std::vector<int> xcords, ycords;	//arrays of live snake block indexes
+		int applex, appley;
+		playField* field;
+		gameState(int, int, playField*);
+		void init();
+		void newApple();
+
+		bool apple=1;
+		bool up = 0;
+		bool down = 0;
+		bool left = 0;
+		bool right = 0;
+		bool gameover = 1;
+		bool message=1;
+		size_t score = 0;
+	
+};	
+
+gameState::gameState(int headx, int heady, playField* field){
+	this->headx = headx;
+	this->heady = heady;
+	this->field = field;
+	newApple();
+}
+
+void gameState::init(){
+	apple=1;
+	up=0;
+	down=0;
+	left=0;
+	right=0;
+	gameover=1;
+	message=1;
+	score = 0;
+	
+}
+
+void gameState::newApple(){
+	this->appley=((rand()%(field->yLen/(blockSize+1)))*(blockSize+1)+(field->limitStarty+1));
+	this->applex=((rand()%(field->xLen/(blockSize+1)))*(blockSize+1)+(field->limitStartx+1));
+}
 
 int main()
 {
 	gfxInitDefault();
 	//gfxSet3D(true);
-	std::vector<int> xcords, ycords;
-	int applex=0;
-	int appley=0;
-	int blocksize=10;
-	int limitstarty=((240%(blocksize+1)-1)/2);
-	int limitstartx=((400%(blocksize+1)-1)/2);
-	int limitendy, limitendx;
-	if((limitstarty*2)<(240%(blocksize+1)-1))
-		limitendy=(239-limitstarty+1);
-	else
-		limitendy=(240-limitstarty);
 
-	if((limitstartx*2)<(400%(blocksize+1)-1))
-		limitendx=(399-limitstartx);
-	else
-		limitendx=(400-limitstartx);
 	
-	int y=limitstarty+1;
-	int x=limitstartx+1;
-	bool apple=1;
-	bool up = 0;
-	bool down = 0;
-	bool left = 0;
-	bool right = 0;
-	bool gameover = 1;
-	bool message=1;
-	size_t score = 0;
+	//int y=limitStarty+1;
+	//int x=limitStartx+1;
 
 	int highscore;
 	std::ifstream readscore;
@@ -104,170 +154,147 @@ int main()
 	gfxSwapBuffers();
 	u8* fb1 = gfxGetFramebuffer(GFX_TOP, GFX_RIGHT, NULL, NULL);
 
-	wall(limitstarty, limitstartx, limitendy, limitendx, fb, fb1);
+	playField field (xRes, yRes);
+	field.init(fb, fb1);
+	gameState game (field.limitStartx+1, field.limitStarty+1, &field);
 	
-	while (aptMainLoop())
+	while (1)
 	{
 		
 		gspWaitForVBlank();
 		hidScanInput();
 
-		u32 kDown = hidKeysDown(); 
-		if(gameover)
+		u32 kDown = hidKeysDown();
+		if(game.gameover)
 		{
 
-			printf("\x1b[0;0Hscore: %d      ", score/2);
+			printf("\x1b[0;0Hscore: %d      ", game.score/2);
 			printf("\x1b[0;15Hhighscore: %d", highscore);
 			//( 240x400 and 240x320) 0x0 - 239x399, 239x319
 
-			xcords.insert(xcords.begin(), x);
-			ycords.insert(ycords.begin(), y);
+			game.xcords.insert(game.xcords.begin(), game.headx);
+			game.ycords.insert(game.ycords.begin(), game.heady);
 
-			if(y<=limitstarty||y>=limitendy-blocksize||x<=limitstartx||x>=limitendx-blocksize)
+			if(game.heady<field.limitStarty||game.heady>field.limitendy-blockSize||game.headx<field.limitStartx||game.headx>field.limitendx-blockSize)	//head of snake has hit border
 			{	
-				gameover=0;
+				game.gameover=0;
 				printf("\x1b[15;6H      Press B to quit       ");
 			}
 
-			if(score>3)
+			if(game.score>3)
 			{
-				for(size_t c = 2;c<xcords.size(); c++)
+				for(size_t c = 2;c<game.xcords.size(); c++)
 				{
-					if(x==xcords.at(c)&&y==ycords.at(c))
+					if(game.headx==game.xcords.at(c)&&game.heady==game.ycords.at(c))
 					{	
-						gameover=0;
+						game.gameover=0;
 						printf("\x1b[15;6H      Press B to quit       ");
 					}
 				}
 			}
 		
-			if(gameover)
+			if(game.gameover)
 			{
 					
-				//snake block
-				for(int c = 0; c<blocksize; c++)
-				{
-					for(int d = 0; d<blocksize; d++)
-					{
-					
-						block(ycords.at(0)+c, xcords.at(0)+d, 1, fb, fb1);
-						if(!message)
-							if(xcords.size()>(score+1))
-								remove(ycords.back()+c, xcords.back()+d, fb, fb1);
+				eraseBlock(game.ycords.back(), game.xcords.back(), fb, fb1);		//erases tail block
 
-					}
-				}
-				while(xcords.size()>(score+1))
-				{
-					xcords.pop_back();
-					ycords.pop_back();
+				eraseBlock(game.ycords.at(0), game.xcords.at(0), fb, fb1);		
+				drawBlock(game.ycords.at(0), game.xcords.at(0), 1, fb, fb1, 0xFF);	//draws new head block
+
+				while(game.xcords.size()>(game.score+1)){
+					game.xcords.pop_back();
+					game.ycords.pop_back();
 				}
 
 
 				//apple block
-				if(apple)
+				if(game.apple)
 				{
 					
-					while(appley<=limitstarty||appley>=limitendy-blocksize||applex<=limitstartx||applex>=limitendx-blocksize)
+					while(game.appley<=field.limitStarty||game.appley>=field.limitendy-blockSize||game.applex<=field.limitStartx||game.applex>=field.limitendx-blockSize)
 					{
-						appley=((rand()%(240/(blocksize+1)))*(blocksize+1)+(limitstarty+1));
-						applex=((rand()%(400/(blocksize+1)))*(blocksize+1)+(limitstartx+1));
-						
-						for(size_t l = 0; l< xcords.size(); l++)
-							if(appley==ycords.at(l)&&applex==xcords.at(l))
+						game.newApple();	
+						for(size_t l = 0; l< game.xcords.size(); l++)
+							if(game.appley==game.ycords.at(l)&&game.applex==game.xcords.at(l))
 								{
-									appley=0;
-									applex=0;
+									game.appley=0;
+									game.applex=0;
 								}
 					}
+
+					drawBlock(game.appley, game.applex, 2, fb, fb1, 0xFF);
 					
-					for(int c = 0; c<blocksize; c++)
-					{
-						for(int d = 0; d<blocksize; d++)
-						{
-							
-							block(appley+c, applex+d, 2, fb, fb1);
-						}
-					}
-					apple=0;
+					game.apple=0;
 					
 				}
 
-				if(x==applex&&y==appley)
+				if(game.headx==game.applex&&game.heady==game.appley)	//apple get!
 				{
-					score+=2;
-					apple=1;
-					applex=0;
-					appley=0;
-					for(int c = 0; c<blocksize; c++)
-					{
-						for(int d = 0; d<blocksize; d++)
-						{
-							remove(y+c, x+d, fb, fb1);
-							block(y+c, x+d, 1, fb, fb1);
-						}
-					}
+					game.score+=2;
+					game.apple=1;
+					game.applex=0;
+					game.appley=0;
+					drawBlock(game.heady, game.headx, 1, fb, fb1, 0xFF);
 				}
 
 				if(kDown)
 				{
-					if(message)
+					if(game.message)
 						for(int c = 0; c<100; c++)
 							printf("\n");
 					
-					message=0;
-					if(((kDown & KEY_UP)||(kDown & KEY_X)) && !down)
+					game.message=0;
+					if(((kDown & KEY_UP)||(kDown & KEY_X)) && !game.down)
 					{	
-						up=1;
-						down=0;
-						left=0;
-						right=0;
+						game.up=1;
+						game.down=0;
+						game.left=0;
+						game.right=0;
 					}
 
-					else if(((kDown & KEY_DOWN)||(kDown & KEY_B)) && !up)
+					else if(((kDown & KEY_DOWN)||(kDown & KEY_B)) && !game.up)
 					{	
-						down=1;
-						up=0;
-						left=0;
-						right=0;
+						game.down=1;
+						game.up=0;
+						game.left=0;
+						game.right=0;
 					}
 
-					else if(((kDown & KEY_RIGHT)||(kDown & KEY_A)) && !left)
+					else if(((kDown & KEY_RIGHT)||(kDown & KEY_A)) && !game.left)
 					{	
-						right=1;
-						up=0;
-						left=0;
-						down=0;
+						game.right=1;
+						game.up=0;
+						game.left=0;
+						game.down=0;
 					}
 
-					else if(((kDown & KEY_LEFT)||(kDown & KEY_Y)) && !right)
+					else if(((kDown & KEY_LEFT)||(kDown & KEY_Y)) && !game.right)
 					{	
-						left=1;
-						up=0;
-						down=0;
-						right=0;
+						game.left=1;
+						game.up=0;
+						game.down=0;
+						game.right=0;
 					}
 				}
 
-				if(left==1)
-					x-=blocksize+1;
-					
+				if(game.left)
+					game.headx-=blockSize+1;
 
-				else if(right==1)
-					x+=blocksize+1;
+				else if(game.right)
+					game.headx+=blockSize+1;
 
-				else if(up==1)
-					y+=blocksize+1;
+				else if(game.up)
+					game.heady+=blockSize+1;
 
-				else if(down==1)
-					y-=blocksize+1;
+				else if(game.down)
+					game.heady-=blockSize+1;
 			}
 		}
 		
 		
 		else
 		{
-			save(score);
+			save(game.score);
 			readscore.open ("snakesave.sav");
 			readscore >> highscore;
 			readscore.close();
@@ -277,24 +304,17 @@ int main()
 				if(kDown & KEY_B)
 					break;
 
-				gameover=1;
-				x=limitstartx+1;
-				y=limitstarty+1;
-				score=0;
-				xcords.clear();
-				ycords.clear();
+				game.gameover=1;
+				game.headx=field.limitStartx+1;
+				game.heady=field.limitStarty+1;
+				game.score=0;
+				game.xcords.clear();
+				game.ycords.clear();
 				
 				memset(fb, 0, 240*400*3);
 				memset(fb1, 0, 240*400*3);
-				wall(limitstarty, limitstartx, limitendy, limitendx, fb, fb1);
-				left=0;
-				right=0;
-				up=0;
-				down=0;
-				apple=1;
-				applex=0;
-				appley=0;
-				message=1;
+				field.init(fb, fb1);
+				game.init();
 				usleep(300000);
 				printf("\x1b[15;6HPress any direction to play");
 			}
